13d3be0025b3d8729b3452b44dab018583b84a9f
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-02 12:38:33 +0100

    Passed tests

--- primes.py
+++ primes.py
@@ -1,6 +1,29 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(number):
+
+    for x in range(2, number):
+        
+        if number%x == 0:
+            return False
+    return True
+                   
+
 def primes(number_of_primes):
-    list = []
-    return list
+
+    if number_of_primes < 1:
+        raise ValueError
+    
+    listOfPrimes = []
+    number = 2
+    
+    while len(listOfPrimes) < number_of_primes:
+        
+        if isPrime(number):
+            listOfPrimes.append(number)
+        number+=1
+
+    return listOfPrimes
+ 
+    
